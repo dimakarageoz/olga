a90e0314a2c2e53fc0899d3f636e09bb4c969377
DimaKarageoz <dkarageoz@gmail.com>
2017-06-10 19:19:05 +0300

    Start

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  module: {
+    loaders:[
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  devServer:{
+    contentBase: __dirname + './src/public',
+    port: 2002
+  },
+  plugins: [
+    new HtmlWebpackPlugin ({
+      template: './src/public/index.html'
+    })
+  ],
+  watch: true
+
+}
